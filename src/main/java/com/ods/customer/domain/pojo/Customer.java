f1d4dcedb1dd7fffad17b6840fc99e784d5c17ea
Nevil Parekh <nparekh127@gmail.com>
2021-08-19 13:10:15 +0530

    added Customer.java

---
+++ src/main/java/com/ods/customer/domain/pojo/Customer.java
@@ -0,0 +1,20 @@
+package com.ods.customer.domain.pojo;
+
+public class Customer {
+
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
